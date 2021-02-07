e44da507542202f289cdbb45dc33629adbf81e97
Otg <otgonjargal.natsag@gmail.com>
2021-02-07 13:47:16 +0800

    first commit

---
+++ docs/js/print.8ac43e6c9818d22c1a17.js
@@ -0,0 +1,1 @@
+(self.webpackChunkTsagaanTolgoi00=self.webpackChunkTsagaanTolgoi00||[]).push([[740],{897:(a,o,s)=>{"use strict";function e(){console.log("I get called from print.js!")}s.d(o,{Z:()=>e})}},0,[[897,666]]]);
